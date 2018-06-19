813eac97f295bcecae642281fe2944c8ab07d558
MAT\jonathan.smith1 <jonathan.smith1@mclaren.com>
2018-06-19 11:26:55 +0100

    Add license type

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	url='https://github.com/jonathan-smith-1/dl_with_numpy',
-	license='',
+	license='MIT License',
 	author='Jonathan Smith',
 	author_email='jhwsmith86@googlemail.com',
 	description='Simple deep learning with numpy',
